89befdc2555ee214ee66ba0cadfd89a3dde175ed
Christian Seidlitz <Christian.Seidlitz@gmx.de>
2016-06-12 13:22:17 +0200

    Updated logging, tried to use DeviceInformation and GattService

--- Flic.Tasks/AdvertisementHandler.cs
+++ Flic.Tasks/AdvertisementHandler.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections;
 using System.Diagnostics;
-
+using System.Linq;
 using Windows.ApplicationModel.Background;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Devices.Bluetooth.Background;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Devices.Enumeration;
 using Windows.Graphics.Printing;
 using Windows.Media.SpeechRecognition;
@@ -29,96 +31,136 @@ namespace Flic.Tasks
             }
         }
 
-        private static async void Print(BluetoothLEAdvertisementReceivedEventArgs advertisementEvent)
+        private static async void Print(BluetoothLEAdvertisementReceivedEventArgs advertisementEvent, int indent = 0)
         {
-            Debug.WriteLine("-- Beacon ----------");
-            Debug.WriteLine(Enum.GetName(typeof(BluetoothLEAdvertisementType), advertisementEvent.AdvertisementType));
-            Debug.WriteLine("BluetoothAddress: {0:X}", advertisementEvent.BluetoothAddress);
-            Debug.WriteLine("Raw Signal Strength In DBm: {0}", advertisementEvent.RawSignalStrengthInDBm);
-            Debug.WriteLine("Timestamp: {0}", advertisementEvent.Timestamp);
+            DebugLine("-- Beacon ----------", indent);
+            DebugLine(Enum.GetName(typeof(BluetoothLEAdvertisementType), advertisementEvent.AdvertisementType), indent);
+            DebugLine($"BluetoothAddress: {advertisementEvent.BluetoothAddress:X}", indent);
+            DebugLine($"Raw Signal Strength In DBm: {advertisementEvent.RawSignalStrengthInDBm}", indent);
+            DebugLine($"Timestamp: {advertisementEvent.Timestamp}", indent);
 
-            
-            Print(advertisementEvent.Advertisement);
+
+            Print(advertisementEvent.Advertisement, indent + 1);
 
             try
             {
                 var device = await BluetoothLEDevice.FromBluetoothAddressAsync(advertisementEvent.BluetoothAddress);
-                Print(device);
+                Print(device, indent + 1);
             }
             catch
             {
-                Debug.WriteLine("Failure getting device from Bluetooth Address.");
+                DebugLine("Failure getting device from Bluetooth Address.", indent);
             }
         }
 
-        private static async void PrintService(Guid serviceGuid)
+        private static async void PrintService(Guid serviceGuid, int indent)
         {
             try
             {
+                DebugLine("BT LE SERVICE LOOKUP", indent);
                 var device = await BluetoothLEDevice.FromIdAsync(serviceGuid.ToString());
-                Print(device);
+                Print(device, indent + 1);
             }
             catch
             {
-                Debug.WriteLine("Failure getting device from service id.");
+                DebugLine("Failure getting device from service id.", indent);
+            }
+
+            try
+            {
+                DebugLine("DEVICE INFORMATION GATT SERVICE LOOKUP", indent);
+                var devices = await DeviceInformation.FindAllAsync(GattDeviceService.GetDeviceSelectorFromUuid(serviceGuid));
+                DebugLine($"Found Devices: {devices.Count}", indent);
+                foreach (var device in devices)
+                {
+                    Print(device, indent + 1);
+                }
+            }
+            catch
+            {
+                DebugLine("Failure getting device from service id.", indent);
             }
 
         }
 
-        private static void Print(BluetoothLEDevice device)
+        private static void Print(BluetoothLEDevice device, int indent)
         {
             var services = device.GattServices;
-            Debug.WriteLine("\t Services:");
+            DebugLine(" Services:", indent);
             foreach (var service in services)
             {
-                Debug.WriteLine($"\t Service {service.Uuid}:");
+                DebugLine($"Service {service.Uuid}:", indent+1);
                 var characteristics = service.GetAllCharacteristics();
-                Debug.WriteLine("\t Characteristics:");
+                DebugLine("Characteristics:", indent+1);
                 foreach (var characteristic in characteristics)
                 {
-                    Debug.WriteLine($"\t\t Char. Description:{characteristic.UserDescription}");
+                    DebugLine($"Char. Description:{characteristic.UserDescription}", indent + 2);
                 }
             }
         }
 
-        private static void Print(BluetoothLEAdvertisement advertisement)
+        private static async void Print(DeviceInformation device, int indent)
         {
-            Debug.WriteLine("Local Name: " + advertisement.LocalName);
-            Debug.WriteLine("Service Uuids:");
+            var service = await GattDeviceService.FromIdAsync(device.Id);
+
+            if (service != null)
+            {
+                DebugLine($"Service {service.Uuid}:", indent);
+                var characteristics = service.GetAllCharacteristics();
+                DebugLine("Characteristics:", indent);
+                foreach (var characteristic in characteristics)
+                {
+                    DebugLine($" Char. Description:{characteristic.UserDescription}", indent + 1);
+                }
+            }
+            else
+            {
+                DebugLine("Access to device is denied. App not granted or in use by other application.", indent);
+            }
+        }
+
+        private static void Print(BluetoothLEAdvertisement advertisement, int indent)
+        {
+            DebugLine("Local Name: " + advertisement.LocalName, indent);
+            DebugLine("Service Uuids:", indent);
 
             var serviceUuids = advertisement.ServiceUuids;
             foreach (var serviceUuid in serviceUuids)
             {
-                Debug.WriteLine($"\t{serviceUuid}");
-                PrintService(serviceUuid);
+                DebugLine($"{serviceUuid}", indent + 1);
+                PrintService(serviceUuid, indent + 2);
             }
 
+            DebugLine("Flags:", indent);
+            DebugLine(advertisement.Flags == null
+                ? "null"
+                : $"(0x{(uint)advertisement.Flags.Value:X}) {Enum.Format(typeof(BluetoothLEAdvertisementFlags), advertisement.Flags, "g")}",
+                indent);
 
-
-            Debug.WriteLine("Flags:");
-            Debug.WriteLine(advertisement.Flags == null
-                ? "\tnull"
-                : $"\t(0x{(uint) advertisement.Flags.Value:X}) {Enum.Format(typeof (BluetoothLEAdvertisementFlags), advertisement.Flags, "g")}");
-
-            Debug.WriteLine("Manufacturer Data:");
+            DebugLine("Manufacturer Data:", indent);
             var manufacturerDatas = advertisement.ManufacturerData;
             foreach (var manufacturerData in manufacturerDatas)
             {
-                Debug.WriteLine($"\tCompany Id {manufacturerData.CompanyId:X}");
-                Debug.WriteLine("\tData:");
+                DebugLine($"Company Id {manufacturerData.CompanyId:X}", indent + 1);
+                DebugLine("Data:", indent + 1);
                 var data = manufacturerData.Data;
-                Debug.WriteLine("\t" + CryptographicBuffer.EncodeToHexString(data));
+                DebugLine(CryptographicBuffer.EncodeToHexString(data), indent + 1);
             }
 
-            Debug.WriteLine("Data Sections:");
+            DebugLine("Data Sections:", indent);
             var dataSections = advertisement.DataSections;
             foreach (var dataSection in dataSections)
             {
-                Debug.WriteLine($"\tDate Type {dataSection.DataType:X}");
-                Debug.WriteLine("\tData:");
+                DebugLine($"Date Type {dataSection.DataType:X}", indent + 1);
+                DebugLine("Data:", indent + 1);
                 var data = dataSection.Data;
-                Debug.WriteLine("\t" + CryptographicBuffer.EncodeToHexString(data));
+                DebugLine(CryptographicBuffer.EncodeToHexString(data), indent + 1);
             }
+        }
+
+        private static void DebugLine(string message, int indent)
+        {
+            Debug.WriteLine(string.Concat(Enumerable.Repeat("    ", indent)) + message);
         }
     }
 }
